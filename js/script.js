de4c3554b9c5a1dba4415e926b98e1a4254e1471
Mel Choyce <mel@skytemple.com>
2012-12-16 22:44:58 -0500

    Fleshing out address book, creating my html/css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
 var address_book = [
 	{
+		name: "Mel Choyce",
+		email: "melissachoyce@gmail.com"
+	},
+	{
 		name: "Kelly Dwan",
 		email: "kelly.dwan@gmail.com"
 	},
 	{
-		name: "Mel Choyce",
-		email: "melissachoyce@gmail.com"
+		name: "Sean Smevik",
+		email: "smsmev@gmail.com"
+	},
+	{
+		name: "Mat Budelman",
+		email: "matbudelman@gmail.com"
+	},
+	{
+		name: "Xin Xin",
+		email: "xin@xin-squared.com"
+	},
+	{
+		name: "Ed Healy",
+		email: "ehealy@gmail.com"
+	},
+	{
+		name: "David Levine",
+		email: "dmlevi@gmail.com"
+	},
+	{
+		name: "Geordie Kaytes",
+		email: "geordie.kaytes@freshtilledsoil.com"
+	},
+	{
+		name: "Time Wright",
+		email: "tim.wright@freshtilledsoil.com"
 	}
 ];
 
