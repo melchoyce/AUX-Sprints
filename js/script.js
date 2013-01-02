3767c4e90ccc7f483249d082481f9cde327a8588
Mel Choyce <mel@skytemple.com>
2013-01-02 16:09:13 -0500

    Finishing up assignment #2
    Added in ajax calls, added aria attributes to html, fixed contacts.json
    to put "" around name/email

--- js/script.js
+++ js/script.js
@@ -1,43 +1,115 @@
+/* =Standard Ajax xhr function to check for XMLHttpRequest vs. XMLHTTP
+-------------------------------------------------------------- */
+
+function getHttpObject() {
+	
+	// initialize the variable
+	var xhr;
+	
+	if (window.XMLHttpRequest) { //check for support
+		
+		// if it's supported, use it because it's better
+		xhr = new XMLHttpRequest();
+		
+	} else if (window.ActiveXObject) { // check for the IE6 Ajax
+		
+		// save it to the xhr var
+		xhr = new ActiveXObject("Msxml2.XMLHTTP");
+	}
+	
+	// spit out the correct one so we can use it
+	return xhr;
+	
+}
+
+
+/* =Define the Ajax Call
+-------------------------------------------------------------- */
+
+/* define the Ajax call function */
+function ajaxCall(dataURL, outputElement, callback) {
+
+	/* use our function to get the current Ajax object based on support */
+	var request = getHttpObject();
+
+	outputElement.innerHTML = "Loading...";
+
+	// check to see if Ajax call went through
+	request.onreadystatechange = function() {
+
+		if (request.readyState === 4 && request.status === 200) {
+
+			//console.log(request.responseText);
+			// save the ajax response to a variable
+			var contacts = JSON.parse(request.responseText);
+
+			// make sure the callback is indeed a function before executing it
+			if (typeof callback === "function") {
+
+				callback(contacts);
+
+			} // end of function check
+
+		} // end ajax status check
+		
+	} // end onreadystatechange
+
+	request.open("GET", dataURL, true);
+	request.send(null);
+	
+} // end function
+
+
+/* =Search Form & Autocomplete
+-------------------------------------------------------------- */
+
 (function () {
 	/* cache some initial variables */
 	var searchForm = document.getElementById("search-form"),
 		searchField = document.getElementsByTagName("input")[0],
-		count = addressBook.length,
 		target = document.getElementById("search-results");
 		
 	/* define address book method */
 	var book = {
 	
 		search : function(event) {
-			// save the input value, contacts length and i to variables
-			var searchValue = searchField.value,
-				i;
-				
-			// stop the default behavior
-			event.preventDefault();
-			
-			// clear the target area just in case there's something in it
-			target.innerHTML = "";
-			
-			// check the count
-			if (count > 0 && searchValue !== "") {
-				
-				// loop through the contacts
-				for(i=0; i < count; i = i+1) {
+		
+			// start the Ajax cal
+			ajaxCall("data/contacts.json", target, function(data) {
+		
+				// save the input value, contacts length and i to variables
+				var searchValue = searchField.value,
+					addrBook = data.addressBook,
+					count = addrBook.length,
+					i;
 					
-					var obj = addressBook[i],
-						isItFound = obj.name.toLowerCase().indexOf(searchValue);
-
-					if(isItFound !== -1) {
-						target.innerHTML += '<a href="mailto:' + obj.email + '"><div class="contact">' + obj.name + '<div class="contact-email"></div></div></a>';
-					} // end if
-
-				} // end for loop
+				// stop the default behavior
+				event.preventDefault();
+				
+				// clear the target area just in case there's something in it
+				target.innerHTML = "";
+				
+				// check the count
+				if (count > 0 && searchValue !== "") {
+					
+					// loop through the contacts
+					for(i=0; i < count; i = i+1) {
+						
+						var obj = addrBook[i],
+							isItFound = obj.name.toLowerCase().indexOf(searchValue);
+	
+						if(isItFound !== -1) {
+							target.innerHTML += '<a href="mailto:' + obj.email + '"><div class="contact">' + obj.name + '<div class="contact-email"></div></div></a>';
+						} // end if
+	
+					} // end for loop
+				
+				} // end if count
 			
-			} // end if count
+			}); // end ajax call
 			
 		} // end search
-
+		
 	} // end book
 	
 	/* activate the event listeners */
