43b7fabfbb9938e5a512fea68b6fd044fbaf8956
Mel Choyce <mel@skytemple.com>
2013-01-04 16:13:59 -0500

    Switching functions to jquery
    Adding jquery to my folder, switching my ajax call and address book
    search functions to jquery from regular js. Still need to switch my
    keyup stuff into jquery.

--- js/script.js
+++ js/script.js
@@ -1,118 +1,56 @@
-/* =Standard Ajax xhr function to check for XMLHttpRequest vs. XMLHTTP
+/* =Define the Ajax Call, then Search Form & Autocomplete
 -------------------------------------------------------------- */
 
-function getHttpObject() {
-	
-	// initialize the variable
-	var xhr;
-	
-	if (window.XMLHttpRequest) { //check for support
-		
-		// if it's supported, use it because it's better
-		xhr = new XMLHttpRequest();
-		
-	} else if (window.ActiveXObject) { // check for the IE6 Ajax
-		
-		// save it to the xhr var
-		xhr = new ActiveXObject("Msxml2.XMLHTTP");
-	}
-	
-	// spit out the correct one so we can use it
-	return xhr;
-	
-}
-
-
-/* =Define the Ajax Call
--------------------------------------------------------------- */
-
-/* define the Ajax call function */
-function ajaxCall(dataURL, outputElement, callback) {
-
-	/* use our function to get the current Ajax object based on support */
-	var request = getHttpObject();
-
-	outputElement.innerHTML = "Loading...";
-
-	// check to see if Ajax call went through
-	request.onreadystatechange = function() {
-
-		if (request.readyState === 4 && request.status === 200) {
-
-			//console.log(request.responseText);
-			// save the ajax response to a variable
-			var contacts = JSON.parse(request.responseText);
-
-			// make sure the callback is indeed a function before executing it
-			if (typeof callback === "function") {
-
-				callback(contacts);
-
-			} // end of function check
-
-		} // end ajax status check
-		
-	} // end onreadystatechange
-
-	request.open("GET", dataURL, true);
-	request.send(null);
-	
-} // end function
-
-
-/* =Search Form & Autocomplete
--------------------------------------------------------------- */
-
+// Wrapping in an anonymous function
 (function () {
+
 	/* cache some initial variables */
-	var searchForm = document.getElementById("search-form"),
-		searchField = document.getElementsByTagName("input")[0],
-		target = document.getElementById("search-results");
-		
-	/* define address book method */
+	var searchField = document.getElementsByTagName("input")[0];
+	
 	var book = {
 	
 		search : function(event) {
 		
-			// start the Ajax cal
-			ajaxCall("data/contacts.json", target, function(data) {
-		
-				// save the input value, contacts length and i to variables
-				var searchValue = searchField.value.toLowerCase(),
+			// stop the default behavior
+            event.preventDefault();
+            
+            // start the Ajax call
+            $.getJSON('data/contacts.json', function(data) {
+	
+				var searchValue = $("#search-field").val().toLowerCase(),
 					addrBook = data.addressBook,
-					count = addrBook.length,
-					i;
-					
-				// stop the default behavior
-				event.preventDefault();
+					count = addrBook.length;
 				
 				// clear the target area just in case there's something in it
-				target.innerHTML = "";
+				$('#search-results').empty();
 				
 				// check the count
 				if (count > 0 && searchValue !== "") {
 					
 					// loop through the contacts
-					for(i=0; i < count; i = i+1) {
-						
-						var obj = addrBook[i],
-							isItFound = obj.name.toLowerCase().indexOf(searchValue);
+					$.each(addrBook, function (i, obj) {
+					
+						// search through whatever is typed to see if they match anything in our contacts lists
+						var isItFound = obj.name.toLowerCase().indexOf(searchValue);
 	
+						// if it's not -1, then it's found some results and will output them in a div
 						if(isItFound !== -1) {
-							target.innerHTML += '<a href="mailto:' + obj.email + '"><div class="contact">' + obj.name + '<div class="contact-email"></div></div></a>';
-						} // end if
-	
-					} // end for loop
+					
+							$('#search-results').append('<a href="mailto:' + obj.email + '"><div class="contact">' + obj.name + '<div class="contact-email"></div></div></a>').fadeIn();
+							
+						}
+					
+					}); // end each
 				
-				} // end if count
+				} // end count check
 			
 			}); // end ajax call
 			
 		} // end search
-		
-	} // end book
+
+	} // end book	
 	
 	/* activate the event listeners */
-	searchField.addEventListener("keyup", book.search, false);
-	
-})();
+    searchField.addEventListener("keyup", book.search, false);
+
+})(); // end anonymous function
